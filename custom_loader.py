469a8c56b9f2867dd2aba2288180c3402ee7a765
Mal-Ourisso <toinho99@gmail.com>
2022-07-15 11:43:53 -0300

    Added image loader

--- custom_loader.py
+++ custom_loader.py
@@ -1,23 +1,25 @@
 
 import numpy as np
-#import tensorflow as tf
-#import keras as kr
+import tensorflow as tf
+import keras as kr
 
 from numpy.random import default_rng
 from time import time
 from glob import glob
 
 rng = default_rng()
-dir_dataset = "/home/mauricio/dados/Mauricio/OCT2017"
-    
+
 def _measureTime(func, *args):
 	start = time()
 	func(*args)
 	return time()-start
 
-def getFileName(directory):
-	return np.array([file_path.split for file_path in glob(directory+"/*.jpeg")])
+def getFileNames(directory):
+	return np.array([file_path for file_path in glob(directory+"/*/*.jpeg")])
 
-def getImageLabel(file_name):
-	pass
-    
+def getImageLabel(file_path):
+	x = tf.keras.utils.load_img(file_path)
+	y = file_path.split("/")[-2]
+	print(y)
+	return x, y
+
